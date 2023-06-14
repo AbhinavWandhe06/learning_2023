b9fde036fb4886429cc0bba8fd582b60a0783545
Abhinav Wandhe <abhinavwandhe@gmail.com>
2023-06-14 14:06:36 +0530

    Updated

--- Module1/day1/if_elseFunction.c
+++ Module1/day1/if_elseFunction.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
-int big(int a, int b) {
+int tebig(int a, int b) {
 return (a > b) ? a : b;
 }
-
+int big(int a, int b) {
+	if (a>b){
+		printf("%d",&a);
+	}
+	else{
+		printf("%d", &b);
+	}
+}
 int main() {
     int num1, num2;
     printf("Enter the first number: ");
@@ -10,7 +17,10 @@ int main() {
     printf("Enter the second number: ");
     scanf("%d", &num2);
 
-    int result = big(num1, num2);
+    int result = tebig(num1, num2);
+    printf("The biggest number is: %d\n", result);
+    
+    int result1 = big(num1, num2);
     printf("The biggest number is: %d\n", result);
 
     return 0;
